482d10a0ce58328d022db04b95608ffebd91f594
JackLi <1319569288@qq.com>
2023-04-02 22:58:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
             <router-link to="/home">首页</router-link>
             <router-link to="/login">登录</router-link>
         </div>
-        <img alt="Vue logo" src="./assets/img/logo.png" />
-        <HelloWorld msg="a coderwhy vue template" />
+        <HelloWorld msg="admin test" />
     </div>
 </template>
 
